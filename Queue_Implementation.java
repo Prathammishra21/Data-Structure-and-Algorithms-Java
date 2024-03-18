4d58dcdbe5ab1fe6142efe337468230e44afedc9
Pratham Mishra <mishrapratham119@gmail.com>
2024-03-18 23:00:21 +0530

    Queue Implementation

---
+++ Queue_Implementation.java
@@ -0,0 +1,74 @@
+public class Queue_Implementation {
+    int size = 1;
+    int[] items = new int[size];
+    int front, rear;
+
+    Queue_Implementation() {
+        front = -1;
+        rear = -1;
+    }
+
+    public boolean isFull() {
+        return front == 0 & rear == size - 1;
+    }
+
+    public boolean isEmpty() {
+        return front == -1;
+    }
+
+
+    public void enqueue(int element) {
+        if (isFull()) {
+            System.out.println("Queue is full");
+        } else {
+            if (front == -1)
+                front = 0;
+            rear++;
+            items[rear] = element;
+            System.out.println("Inserted " + element);
+        }
+    }
+
+    public int dequeue() {
+        int element;
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return (-1);
+        } else {
+            element = items[front];
+            if (front >= rear) {
+                front = -1;
+                rear = -1;
+            } else {
+                front++;
+            }
+            System.out.println("Deleted -> " + element);
+            return (element);
+        }
+    }
+
+    public void display() {
+        int i;
+        if (isEmpty()) {
+            System.out.println("Empty Queue");
+        } else {
+            System.out.println("\nFront index-> " + front);
+            System.out.println("Items -> ");
+            for (i = front; i <= rear; i++)
+                System.out.print(items[i] + "  ");
+
+            System.out.println("\nRear index-> " + rear);
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue_Implementation queue = new Queue_Implementation();
+        queue.enqueue(12);
+        queue.enqueue(21);
+        queue.enqueue(19);
+        queue.enqueue(96);
+        queue.enqueue(56);
+        queue.dequeue();
+        queue.display();
+    }
+}
--- Stack_Implementation.java
+++ Stack_Implementation.java
@@ -6,7 +6,7 @@ public class Stack_Implementation {
     private static int capacity;
 
     Stack_Implementation(int size) {
-        arr = new int[size];
+        arr = new int[size];                         //  Creating a stack
         capacity = size;
         top = -1;
     }
@@ -14,7 +14,7 @@ public class Stack_Implementation {
     public static void push(int x) {
         if (isFull()) {
             System.out.println("Overflow");
-            System.exit(1);
+            System.exit(1);                   // Add elements into stack
         } else {
             System.out.println("Inserting " + x);
             arr[++top] = x;
@@ -23,26 +23,26 @@ public class Stack_Implementation {
 
     public static int pop() {
         if (isEmpty()) {
-            System.out.println("Stack Empty");
+            System.out.println("Stack Empty");      // Remove elements from stack
             System.exit(1);
         }
         return arr[top--];
     }
 
     public int size() {
-        return top + 1;
+        return top + 1;                            // Function to return size of stack
     }
 
     private static boolean isEmpty() {
-        return top == -1;
+        return top == -1;                          // Check if stack is empty
     }
 
     private static boolean isFull() {
-        return top == capacity - 1;
+        return top == capacity - 1;                // Check if stack is full
     }
 
     public static void printStack() {
-        for (int i = 0; i <= top; i++) {
+        for (int i = 0; i <= top; i++) {          // using loop for printing stack
             System.out.println(arr[i]);
         }
     }
@@ -54,12 +54,11 @@ public class Stack_Implementation {
         Stack_Implementation.push(51);
         Stack_Implementation.push(144);
         Stack_Implementation.push(132);
-
         Stack_Implementation.printStack();
 
         int poppedelement = Stack_Implementation.pop();
         System.out.println("Popped element: " + poppedelement);
-        System.out.println(" After Popping element");
+        System.out.println("After Popping element: ");
 
         Stack_Implementation.printStack();
     }
