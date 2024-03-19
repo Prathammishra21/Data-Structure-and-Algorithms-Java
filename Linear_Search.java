4d0813c0cf1b8b30205bfedd1716ae1f8d056aa4
Pratham Mishra <mishrapratham119@gmail.com>
2024-03-19 23:06:55 +0530

    Linear Search Implementation

---
+++ Linear_Search.java
@@ -0,0 +1,22 @@
+public class Linear_Search {
+    static void linear_search(int[] a, int n, int key) {
+        int i, count = 0;
+        for(i = 0; i < n; i++) {
+            if(a[i] == key) {
+                System.out.println("The element is found at " + (i+1)+ " index");
+                count = count + 1;
+            }
+        }
+        if(count == 0) // for unsuccessful search
+            System.out.println("The element is not present in the array");
+    }
+    public static void main(String[] args) {
+        int  n, key;
+        n = 6;
+        int[] a = {12, 44, 32, 18, 4, 10, 66};
+        key = 10;
+        linear_search(a, n, key);
+        key = 54;
+        linear_search(a, n, key);
+    }
+}
